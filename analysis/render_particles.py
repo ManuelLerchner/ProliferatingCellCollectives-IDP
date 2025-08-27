e037564eedcaa8c1e8a147444bd762549bbe62ef
ManuelLerchner <manuel.lerchner1111@gmail.com>
2025-08-27 16:26:24 +0200

    adapt models

---
+++ analysis/render_particles.py
@@ -0,0 +1,38 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+
+# --- Visualization setup ---
+fig, ax = plt.subplots(figsize=(6, 6))
+
+ax.set_aspect('equal')
+
+
+def draw_particles(data):
+    ax.clear()
+
+    ax.set_aspect('equal')
+
+    # color scale from 1 to 2 - dark green to light green
+    cmap = mpl.colors.LinearSegmentedColormap.from_list(
+        'custom_cmap', [(0, 0.2, 0), (0, 1, 0)])
+    norm = mpl.colors.Normalize(vmin=1, vmax=2)
+
+    for i, row in data.iterrows():
+        x, y = row['x'], row['y']
+        angle = row['orientation_angle'] * 180 / np.pi
+        length = row['lengths_x']
+
+        fill_color = cmap(norm(row['lengths_x']))
+
+        # ellipse
+        ellipse = mpl.patches.Ellipse(
+            (x, y), length, 0.5, angle=angle, fill=True, fc=fill_color, ec="green", lw=2.0)
+        ax.add_patch(ellipse)
+
+    # reset x and y limits
+    ax.set_xlim(data['x'].min()-5, data['x'].max()+5)
+    ax.set_ylim(data['y'].min()-5, data['y'].max()+5)
+
+    plt.pause(0.01)
