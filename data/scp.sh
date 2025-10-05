5512189f07a3257af63a354282753df4ed6e0978
ManuelLerchner <manuel.lerchner1111@gmail.com>
2025-10-05 04:31:40 +0200

    create plots

--- data/scp.sh
+++ data/scp.sh
@@ -1,5 +1,5 @@
 FOLDER=/dss/dsshome1/01/ge47wer3/build/src/growth_comparison_data/
-FOLDER=/dss/dsshome1/01/ge47wer3/build/src/vtk_output_soft/
+# FOLDER=/dss/dsshome1/01/ge47wer3/build/src/lambda_grow/
 CURR_TIME=$(date +"%Y%m%d_%H%M%S")/
 
 # scp -r ge47wer3@cool.hpc.lrz.de:$FOLDER $CURR_TIME
