b0ea7a78878c152fa995aaebd566b509d13fa855
ManuelLerchner <manuel.lerchner1111@gmail.com>
2025-10-10 00:03:22 +0200

    update strong scaling images

--- data/scp.sh
+++ data/scp.sh
@@ -1,4 +1,4 @@
-FOLDER=/dss/dsshome1/01/ge47wer3/build/src/huge/
+FOLDER=/dss/dsshome1/01/ge47wer3/build/src/strong_scaling/1759615720894.031/output_soft/24ranks
 # FOLDER=/dss/dsshome1/01/ge47wer3/build/src/lambda_grow/
 CURR_TIME=$(date +"%Y%m%d_%H%M%S")/
 
